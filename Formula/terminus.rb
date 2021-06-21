3ae3bcb365a08f77f25977912b314043e76d43ab
stovak <119924+stovak@users.noreply.github.com>
2021-06-21 13:20:44 -0700

    formula update

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -15,25 +15,12 @@ class Terminus < Formula
 
   def install
     ENV.deparallelize
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "composer install"
     #system "composer phar:build"
     bin.install "t3" => "t3"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test terminus#v`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
+    system "touch t3"
   end
 end
